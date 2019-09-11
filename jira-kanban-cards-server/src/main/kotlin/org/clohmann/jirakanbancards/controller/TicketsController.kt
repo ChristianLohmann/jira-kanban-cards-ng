6d365b999ea0ac74f175d2e0e7ca2309b5b4ae54
Christian Lohmann <der.chris@zalando.de>
2019-09-11 18:33:48 +0200

    fixed controller

--- jira-kanban-cards-server/src/main/kotlin/org/clohmann/jirakanbancards/controller/TicketsController.kt
+++ jira-kanban-cards-server/src/main/kotlin/org/clohmann/jirakanbancards/controller/TicketsController.kt
@@ -16,70 +16,72 @@ import org.zalando.riptide.Http
 @RestController
 class TicketsController(
     @Qualifier("jira") private val http: Http
-)
+) {
 
-
-@PostMapping("/search")
-fun getTicketsByJql(@RequestBody searchRequest: SearchRequest): List<Ticket> {
-    return mutableListOf<Ticket>(
-        Ticket(
-            id = 100000001,
-            key = "BOOM-1",
-            self = "https://url-to-jira.com/rest/api/latest/issue/100000001",
-            expand = "operations,versionedRepresentations,editmeta,changelog,renderedFields",
-            fields = Fields(
-                issuetype = IssueType(
-                    id = "1",
-                    description = "A task that needs to be done.",
-                    iconUrl = "https://url-to-jira.com/secure/viewavatar?size=xsmall&avatarId=15778&avatarType=issuetype",
-                    name = "Task",
-                    subtask = false
-                ),
-                summary = "Do that thing!",
-                status = Status(
-                    description = "Specification in progress",
-                    name = "Initial"
-                ),
-                priority = Priority(
-                    iconUrl = "https://url-to-jira.com/images/icons/priorities/major.svg",
-                    name = "Major"
-                ),
-                reporter = Reporter(
-                    name = "dhoppenstedt",
-                    displayName = "Dickie Hoppenstedt"
-                ),
-                assignee = null
-            )
-        ),
-        Ticket(
-            id = 100000002,
-            key = "BOOM-2",
-            self = "https://url-to-jira.com/rest/api/latest/issue/100000002",
-            expand = "operations,versionedRepresentations,editmeta,changelog,renderedFields",
-            fields = Fields(
-                issuetype = IssueType(
-                    id = "1",
-                    description = "A bug that needs to be fixed.",
-                    iconUrl = "https://url-to-jira.com/secure/viewavatar?size=xsmall&avatarId=15458&avatarType=issuetype",
-                    name = "Bug",
-                    subtask = false
-                ),
-                summary = "Fix that thing!",
-                status = Status(
-                    description = "Work in progress",
-                    name = "In Progress"
-                ),
-                priority = Priority(
-                    iconUrl = "https://url-to-jira.com/images/icons/priorities/major.svg",
-                    name = "Major"
-                ),
-                reporter = Reporter(
-                    name = "ohoppenstedt",
-                    displayName = "Opa Hoppenstedt"
-                ),
-                assignee = null
+    @PostMapping("/search")
+    fun getTicketsByJql(@RequestBody searchRequest: SearchRequest): List<Ticket> {
+        return mutableListOf<Ticket>(
+            Ticket(
+                id = 100000001,
+                key = "BOOM-1",
+                self = "https://url-to-jira.com/rest/api/latest/issue/100000001",
+                expand = "operations,versionedRepresentations,editmeta,changelog,renderedFields",
+                fields = Fields(
+                    issuetype = IssueType(
+                        id = "1",
+                        description = "A task that needs to be done.",
+                        iconUrl = "https://url-to-jira.com/secure/viewavatar?size=xsmall&avatarId=15778&avatarType=issuetype",
+                        name = "Task",
+                        subtask = false
+                    ),
+                    summary = "Do that thing!",
+                    status = Status(
+                        description = "Specification in progress",
+                        name = "Initial"
+                    ),
+                    priority = Priority(
+                        iconUrl = "https://url-to-jira.com/images/icons/priorities/major.svg",
+                        name = "Major"
+                    ),
+                    reporter = Reporter(
+                        name = "dhoppenstedt",
+                        displayName = "Dickie Hoppenstedt"
+                    ),
+                    assignee = null
+                )
+            ),
+            Ticket(
+                id = 100000002,
+                key = "BOOM-2",
+                self = "https://url-to-jira.com/rest/api/latest/issue/100000002",
+                expand = "operations,versionedRepresentations,editmeta,changelog,renderedFields",
+                fields = Fields(
+                    issuetype = IssueType(
+                        id = "1",
+                        description = "A bug that needs to be fixed.",
+                        iconUrl = "https://url-to-jira.com/secure/viewavatar?size=xsmall&avatarId=15458&avatarType=issuetype",
+                        name = "Bug",
+                        subtask = false
+                    ),
+                    summary = "Fix that thing!",
+                    status = Status(
+                        description = "Work in progress",
+                        name = "In Progress"
+                    ),
+                    priority = Priority(
+                        iconUrl = "https://url-to-jira.com/images/icons/priorities/major.svg",
+                        name = "Major"
+                    ),
+                    reporter = Reporter(
+                        name = "ohoppenstedt",
+                        displayName = "Opa Hoppenstedt"
+                    ),
+                    assignee = null
+                )
             )
         )
-    )
+
+    }
+
 
 }
